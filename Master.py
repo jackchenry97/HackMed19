84bbbf7294d5242a53e9666a68672b367a534b14
jackchenry97 <37241382+jackchenry97@users.noreply.github.com>
2019-03-30 15:40:42 +0000

    1

--- Master.py
+++ Master.py
@@ -12,7 +12,7 @@ def main():
     print("loading")
     return render_template('main.html')
 
-@app.route('/sendMessage/',methods=["POST"])
+@app.route('/sendMessage',methods=["POST"])
 def sendMessage():
     print("Running")
     responseData = client.send_message(
@@ -21,7 +21,9 @@ def sendMessage():
         "to": 447591608879,
         "text": "A text message sent using the Nexmo SMS API",
     })
-    return render_template('main.html')
+    return "okay"
+
+
 
 if __name__ == '__main__':
     app.run()
