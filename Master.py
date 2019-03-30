5522df45df32ebc624089ae4a0a0022e59e1e946
jackchenry97 <37241382+jackchenry97@users.noreply.github.com>
2019-03-30 14:40:47 +0000

    Update

--- Master.py
+++ Master.py
@@ -1,6 +1,27 @@
 from flask import Flask, render_template, request
 import nexmo
 
-@app.route('/main',methods=["GET"])
+client = nexmo.Client(key="5827c039", secret="t6ixFVYXNDfwjNd8")
+
+app = Flask(__name__)
+
+app.debug = True
+
+@app.route('/',methods=["GET"])
 def main():
+    print("loading")
     return render_template('main.html')
+
+@app.route('/sendMessage/',methods=["POST"])
+def sendMessage():
+    print("Running")
+    responseData = client.send_message(
+    {
+        "from": "HackMed19",
+        "to": 447591608879,
+        "text": "A text message sent using the Nexmo SMS API",
+    })
+    return render_template('main.html')
+
+if __name__ == '__main__':
+    app.run()
