6adb548b32ac8652bdd87ef79f545de46cb1f9a5
jackchenry97 <37241382+jackchenry97@users.noreply.github.com>
2019-03-30 16:51:28 +0000

    1

--- Master.py
+++ Master.py
@@ -16,29 +16,33 @@ def main():
     print("loading")
     
     return render_template('main.html')
-    
-
-@app.route('/webhooks/outbound-sms',methods=["GET", "POST"])
-def sendMessage():
-    responseData = client.send_message(
-    {
-        "from": "HackMed19",
-        "to": 447591608879,
-        "text": "What",
-    })
-    return ("okay")
-
 
 
 @app.route('/webhooks/inbound-sms', methods=['GET', 'POST'])
 def inbound_sms():
     if request.is_json:
         pprint(request.get_json())
+        print("json")
     else:
         data = dict(request.form) or dict(request.args)
         pprint(data)
+        print(data)
     
     return ('', 204)
+
+@app.route('/webhooks/outbound-sms',methods=["GET", "POST"])
+def sendMessage():
+    responseData = client.send_message(
+    {
+        "from": "HackMed19",
+        "to": "447591608879",
+        "text": "What",
+    })
+    return ("okay")
+
+
+
+
 
 if __name__ == '__main__':
     app.run(port=5000)
