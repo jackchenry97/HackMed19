24c6206ed441e672f6747960ab50b9d89198053b
jackchenry97 <37241382+jackchenry97@users.noreply.github.com>
2019-03-30 19:36:05 +0000

    Messages

--- Master.py
+++ Master.py
@@ -1,55 +1,74 @@
 from flask import Flask, render_template, request
 import nexmo
-from pprint import pprint
 
 
 client = nexmo.Client(key="5827c039", secret="t6ixFVYXNDfwjNd8")
 
 app = Flask(__name__)
-
+print("loading")
 app.debug = True
 
-print("OK")
 
 @app.route('/',methods=["GET"])
-def main():
-    print("loading")
-    
+def main():  
     return render_template('main.html')
-    
+
+@app.route('/test',methods=["GET"])
+def test():
+    print('test')
+    return 'test'
+
+callHistory = {}
+
+@app.route('/webhooks/inbound-sms', methods=['GET', 'POST'])
+def inbound_sms():
+    print('Recieved a message')
+    data = request.args
+    print(data)
+
+    inbound = str(data.get("msisdn"))
+    text = data.get("text")
+    print(inbound)
+
+    if  inbound not in callHistory:
+        callHistory[inbound] = 1
+    else:
+        callHistory[inbound] += 1
+    identify_sender(inbound, text) 
+    return ('', 204)
+
+
+people = {}
+first_message = "Please reply with your name"
+second_message = "Lets begin the game, reply to start"
+third_message = "Third message"
+
+def identify_sender(inbound, text):
+    print(inbound)
+    if callHistory[inbound] == 1:
+        message(inbound, first_message)
+    elif callHistory[inbound] == 2:
+        people[inbound] = text
+        message(inbound, second_message)
+    elif callHistory[inbound] >= 3:
+        message(inbound, third_message)
 
 @app.route('/webhooks/outbound-sms',methods=["GET", "POST"])
-def sendMessage():
+def message(number, message):
+    print("sending " + message + " to " + number)
     responseData = client.send_message(
     {
         "from": "HackMed19",
-        "to": 447591608879,
-        "text": "What",
+        "to": number,
+        "text": message,
     })
-    return ("okay")
+    print(responseData)
+    return ("Sent")
 
 
 
-@app.route('/webhook/inbound-sms', methods=['GET', 'POST'])
-def inbound_sms():
-    print("Method is working")
-    if request.is_json:
-        print("Its a json")
-        print(request.get_json())
-    else:
-        data = dict(request.form) or dict(request.args)
-        
-        print("Sender Number:")
-        print(data.get("msisdn"))
-        
-        
-        print("Text message:")
-        print(data.get("text"))
-        # print("Its not a json")
-        # pprint(data)
-    
-    return ('', 204)
+
 
 if __name__ == '__main__':
 
-    app.run(port=3000)
+    app.run(port=5000)
