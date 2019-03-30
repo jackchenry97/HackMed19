4fe3cf5e492e71240f542a5041eef836a0f77a37
jackchenry97 <37241382+jackchenry97@users.noreply.github.com>
2019-03-30 20:56:49 +0000

    Change

--- Master.py
+++ Master.py
@@ -3,14 +3,15 @@ import nexmo
 
 
 client = nexmo.Client(key="5827c039", secret="t6ixFVYXNDfwjNd8")
+Q = {}
+A = {}
 
 def read_QA(filename = "questions.csv"):
   # reads 1st col as questions
   # and second col as answer
   # return array with matrix[0][0...n] with questions
   # and matrix[1][0...n] with respective answers
-  Q = {}
-  A = {}
+
   import csv
   with open(filename) as csvfile:
     spamreader = csv.reader(csvfile, delimiter=',')
@@ -19,6 +20,7 @@ def read_QA(filename = "questions.csv"):
       Q[index] = row[0]
       A[index] = row[1]
       index = index + 1
+      #count how many rows in csv
   return(Q,A)
 QA_matrix = read_QA(filename="questions.csv")
 
@@ -76,10 +78,9 @@ wrong_answer = "Please try again!"
 people = {}
 first_message = "Please reply with your name"
 second_message = "Lets begin the game!"
-third_message = "First Question: Blah Blah Blah."
-third_answers = "Reply with the letter for the correct answer: A: Apples, B:Monty C:Fall over"
 
 end_message = "You have finished the game"
+
 
 def identify_sender(inbound, text):
     print(callHistory[inbound])
@@ -88,13 +89,13 @@ def identify_sender(inbound, text):
     elif callHistory[inbound] == 2:
         people[inbound] = text
         message(inbound, second_message)
-        message(inbound, third_message)
-        message(inbound, third_answers)
-    elif callHistory[inbound] == 3:
-        if text == "A" or "B":
-            message(wrong_answer)
+        message(inbound, Q[0])
+    elif callHistory[inbound] >= 3:
+        question_index = callHistory[inbound] -3
+        if text == A[question_index]:
+            message(inbound, correct_answer)
         else:
-            message(correct_answer)
+            message(inbound, wrong_answer)
 
 
 
@@ -108,7 +109,7 @@ def message(number, message):
     print("sending " + message + " to " + number)
     responseData = client.send_message(
     {
-        "from": "HackMed19",
+        "from": "447937946988",
         "to": number,
         "text": message,
     })
